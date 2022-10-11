89e1ee82945b60214cfa1f012a88284f1390f8c6
Keenly <keenlyhere@gmail.com>
2022-10-11 15:38:31 -0700

    11 solution

--- problems/11-flatten.js
+++ problems/11-flatten.js
@@ -1,6 +1,6 @@
 /***********************************************************************
 Write a recursive function called `flatten` that takes a single array with
-any number of nested arrays and returns and array with all the nested
+any number of nested arrays and returns an array with all the nested
 contents on one level.
 
 Do not use the built in Array.flat.
@@ -12,10 +12,41 @@ flatten([1, 2]); // [1, 2]
 flatten([1, [2, [3]]]); // [1, 2, 3]
 ***********************************************************************/
 
-function flatten(arr) {
-  // Your code here
+function flatten(arr, newArray = []) {
+  if (arr.length === 0) {
+    return newArray;
+  }
+  if (Array.isArray(arr[0])) {
+    newArray = newArray.concat(flatten(arr[0]));
+  } else {
+    newArray.push(arr[0])
+  }
+
+  return flatten(arr.slice(1), newArray)
 }
-  
+
+// function flatten(arr) {
+//   let newArray = [];
+
+//   function helper (arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//       const element = arr[i];
+
+//       if (Array.isArray(element)) {
+//         helper(element);
+//       } else {
+//         newArray.push(element)
+//       }
+//     }
+//   }
+
+//   helper(arr);
+//   return newArray;
+// }
+
+console.log(flatten([])); // []
+console.log(flatten([1, 2])); // [1, 2]
+console.log(flatten([1, [2, [3]]])); // [1, 2, 3]
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 module.exports = flatten;
-  
