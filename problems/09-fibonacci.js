1d980ac960de67e0c9a06d90e4cdf2671ecde397
Keenly <keenlyhere@gmail.com>
2022-10-11 14:45:52 -0700

    09 solution

--- problems/09-fibonacci.js
+++ problems/09-fibonacci.js
@@ -20,9 +20,20 @@ fibonacci(10); // 55
 ***********************************************************************/
 
 function fibonacci(n) {
-  // Your code here
+  if (n === 0) {
+    return 0;
+  } else if (n === 1) {
+    return 1;
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
 }
-  
+
+console.log(fibonacci(1)); // 1
+console.log(fibonacci(2)); // 1
+console.log(fibonacci(3)); // 2
+console.log(fibonacci(4)); // 3
+console.log(fibonacci(10)); // 55
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 module.exports = fibonacci;
-  
