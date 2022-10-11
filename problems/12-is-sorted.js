f2b76367415b0496f0522bffdce42be924c9ae4a
Erik <spike158899@gmail.com>
2022-10-11 15:44:27 -0700

    solution 12

--- problems/12-is-sorted.js
+++ problems/12-is-sorted.js
@@ -12,7 +12,15 @@ isSorted([5, 4, 3, 2, 1]); // false
 
 
 function isSorted(arr) {
-  // Your code here
+  if(arr.length === 0){
+    return true;
+  } else {
+    if(arr[1] < arr[0]){
+      return false;
+    } else {
+      return isSorted(arr.slice(1));
+    }
+  }
 }
 
 
